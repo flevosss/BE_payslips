40a2862288dcd4c1ea6473532ef0ad8ddff2d921
flevosss <michail@flevaris.gr>
2025-10-15 22:03:41 +0200

    fixed deployment

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -11,9 +11,3 @@ bundle exec rails tailwindcss:build
 # Precompile assets
 bundle exec rails assets:precompile
 
-# Run database migrations
-bundle exec rails db:migrate
-
-# Seed the database (optional - remove if you don't want to seed in production)
-# bundle exec rails db:seed
-
